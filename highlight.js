3c7201f904d834bda81927c03348722dc5f355e9
Vinícius Silva <vinicius768@msn.com>
2018-03-27 14:57:59 -0300

    Development

--- highlight.js
+++ highlight.js
@@ -7,11 +7,14 @@ window.addEventListener('load', function () {
     highlight("print");
     highlight("print_r");
     highlight("var_dump");
+
+    var nodesHighlighted = document.getElementsByClassName("gh-debug-finder-highlighted");
+    console.log(nodesHighlighted.length);
 });
 
 function highlight(text) {
     document.body.innerHTML = document.body.innerHTML.replace(
         new RegExp(text, 'gi'),
-        '<b style="background-color:#ff0;font-size:100%">$&</b>'
+        '<span class="gh-debug-finder-highlighted" style="background-color:#ff0;font-size:100%">$&</span>'
     );
 }
